1fdc9c626b901701f99c9081f0f59ea7ed3c7b35
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-25 13:35:38 +0530

    create header in index page

---
+++ asserts/header.php
@@ -0,0 +1,22 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>EUROFLORA</title>
+</head>
+<body>
+    <header>
+        <img src="images/logo-small.png" alt="log image">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contact.php">Contact us</a></li>
+            <li><a href="florist.php">The florist</a></li>
+            <li><a href="gallary.php">Gallary</a></li>
+            <li><a href="order.php">How to order</a></li>
+            <li><a href="phone.php">Order by phone</a></li>
+            <li><a href="chat.php">Chat</a></li>
+        </ul>
+    </header>
+    
+</body>
