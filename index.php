040c8d2a50237682e7f33f7b30d849054de9864c
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-25 14:21:44 +0530

    create image card show

--- index.php
+++ index.php
@@ -2,4 +2,37 @@
 include("asserts/header.php");
 ?>
 
+<link rel="stylesheet" href="style/style.css">
+<div style="background:gray; padding: 5px">
+    <div class="pageView">
+    <br><br><br><br>
+        <h2><b>Malshan Flora</b> your online Florist to deliver Flowers in Kotikawatta
+        </h2>
+        <div>
+            <img src="images/euroflora-florist-flower-bouquet.jpg" alt="photo" style="width:76%; float: left;">
+            <img src="images/Cheyanne.jpg" alt="photo" style="width:24%;float: right;">
+        </div>
+
+        <br>
+
+        <hr>
+        <hr>
+
+        <p style="padding: 10px">We offer a same day Flowers and Plants delivery service in Kotikawatta even in a few hours.
+        From our online Flower shop can order nice Bouquets and Flowers Gifts quickly and easily.
+        Choose your Bouquet from 68 € delivery included. Payments accepted: Credit Card and Paypal.
+        </p>
+
+        <div class="card">
+            <img src="images/alexa.jpg" alt="image">
+            <h2>Alexa</h2>
+            <p>This is the test</p>
+            <span class="priceTag">From 80.00 €</span>
+        </div>
+
+
+    </div>
+</div>
+
+
 
